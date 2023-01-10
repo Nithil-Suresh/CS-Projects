589896df967c612dac20229a8629822a784b2eec
Nithil-Suresh <81449488+Nithil-Suresh@users.noreply.github.com>
2023-01-10 13:05:54 -0500

    Update BigOh.java

--- Big Oh Theory/BigOh.java
+++ Big Oh Theory/BigOh.java
@@ -6,14 +6,11 @@ import java.util.Vector;
 public class BigOh {
 	public static void main(String [] args) {  
 	}
-	// Note: that each method has a default return value, you *must* change 
-	// this as you write your solution (i.e., don’t keep “return 0”, for example, 
-	// change it to the appropriate thing to return based on the method).  
+  
 	
 	/* Binary Search: Should run in Theta(logn) time */
 	/* Returns true if item is in the array a */
 	public static boolean binarySearch(int[] a, int item) {
-		//TODO: IMPLEMENT THIS METHOD
 		//Set int variablew "min" to zero, and "max" to a.length-1 (last index)
 		// Continue while min is less than or equal to max
 			//middle = middle element between min and max [Do this: (min+max)/2;]
@@ -44,7 +41,6 @@ public class BigOh {
 	
 	/* Max value in array: Should run in Theta(n) time */
 	public static int max(int[] a) {
-		//TODO: IMPLEMENT THIS METHOD
 		//Remember to use a for-loop here!
 		int max = a[0];
 		for( int i = 0; i < a.length; i++ ) {
@@ -57,10 +53,8 @@ public class BigOh {
 
 	
 	/* Calls binary search n times. Counts number of successful searches */
-	/* You should search for the numbers 1 through n in succession */
 	/* Should run in Theta(nlogn) time */
 	public static int multipleBinarySearch(int[] a) {
-		//TODO: IMPLEMENT THIS METHOD
 		//Create a for-loop that runs n times, where n is the size of the input array
 	    	//  Updated 10/7: search for each value a[i] in the array in succession
 		//  Call binarySearch within the loop, if it returns true, increment a counter
@@ -79,7 +73,6 @@ public class BigOh {
 	/* Counts pairs of numbers whose sum is multiple of 5 */
 	/* Should run in Theta(n^2) time */
 	public static int allPairs(int[] a) {
-		//TODO: IMPLEMENT THIS METHOD
 		//Create two (2) nested for-loops that go from 0 to the end of the array
 		//These become two indices into the array (use to compare two elements)
 		//You now are able to compare all pairs, check if the sum of two numbers is a 
@@ -100,7 +93,6 @@ public class BigOh {
 	/* Counts the pairs of three in the list a,b,c in which a+b=c */
 	/* Should run in Theta(n^3) time */
 	public static int allTriads(int[] a) {
-		//TODO: IMPLEMENT THIS METHOD
 		//Similar strategy to the "allPairs method.
 		//Create three (3) nested for-loops
 		//Comparing three items in the list (a, b, and c), check if a+b=c. 
